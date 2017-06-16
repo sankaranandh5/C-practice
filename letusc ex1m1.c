a67c3727500fd63c08c2e040bfcc6198274f9fe0
sankaranandh <sankarsanandh@gmail.com>
2017-06-16 23:43:16 +0530

    fifth commit

---
+++ letusc ex1m1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main ()
+{
+/*Declaring and initializing variables*/
+    int value,dig1,dig2,dig3,dig4,dig5,temp;
+
+/*prompt to take the input*/
+    printf("Please enter a five-digit number: ");
+
+/*Taking input*/
+    scanf("%d",&value);
+    temp = value ;
+
+/*Calculations*/
+    //separating digits
+    dig1 = value % 10;
+    value = value / 10;
+    dig2 = value % 10;
+    value = value / 10;
+    dig3 = value % 10;
+    value = value / 10;
+    dig4 = value % 10;
+    value = value / 10;
+    dig5 = value % 10;
+
+    //adding 1 to each digit
+    dig1 = ((dig1+1)%10);
+    dig2 = ((dig2+1)%10);
+    dig3 = ((dig3+1)%10);
+    dig4 = ((dig4+1)%10);
+    dig5 = ((dig5+1)%10);
+
+/*showing Results*/
+    printf("\n   Solution: ");
+    printf("\n   __________");
+    printf("\n\n   By adding one in each digit of %d it becomes : %d%d%d%d%d",temp,dig5,dig4,dig3,dig2,dig1);
+
+   return 0;
+}
